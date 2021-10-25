fb731fda5646781ea4b2696c0ecde89faf4ba190
mrk4009 <kdcsilk@gmail.com>
2021-10-26 00:46:09 +0900

    test

--- 1stPython.py
+++ 1stPython.py
@@ -4,3 +4,11 @@ print('hello world!')
 print('git check')
 
 print('branch test')
+name = 'test';
+age = '14'
+
+
+print('age'+age+'test'+name)
+print('age'+age+'test'+name)
+print('age'+age+'test'+name)
+
